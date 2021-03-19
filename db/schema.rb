151a2f0f5a7e22224d5d0776ccaa6b4d935d86d8
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-03-19 14:34:03 +0900

    add validate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_002841) do
+ActiveRecord::Schema.define(version: 2021_03_15_161618) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
