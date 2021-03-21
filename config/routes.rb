c6a6570809ac834dec22dc51ca94c5b4d6a394ff
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-03-21 14:39:00 +0900

    add login/logout create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get '/login', to: 'sessions#new'
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'session#create'
+  # delete '/logout', to: 'session#destroy'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   namespace :admin do
     resources :users
