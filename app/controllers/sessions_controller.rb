cfef09e1f67703f4d0ee49a8348e78527df2bb86
saeyama <camper_green_yellow_21@yahoo.co.jp>
2021-03-27 08:35:14 +0900

    add current_user/scope/rails_autolink

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  skip_before_action :login_required
+  
   def new
   end
 
   def create
     user = User.find_by(email: session_params[:email])
 
-    if user&.authenticate(session_params[:password])
+    if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'ログインしました。'
     else
